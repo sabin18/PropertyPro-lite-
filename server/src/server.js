5362d246d65563403a00c058504e42d00a637031
Lukaskov <sabin6341@gmail.com>
2019-07-07 12:18:07 +0200

    [ft-#167106462] update server

--- server/src/server.js
+++ server/src/server.js
@@ -10,50 +10,7 @@ app.use(express.urlencoded({ extended:false }));
 app.use(express.json());
 
 app.use(router);
-
-// enable files upload
-/*router.use(fileUpload({
-  createParentPath: true,
-  useTempFiles:true
-}));
-
-/*configure our cloudinary
-cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME, 
-  api_key: process.env.API_ID, 
-  api_secret: process.env.API_SECRET 
-});
-*/
-//add other middleware
-//app.use(bodyParser.json());
-//app.use(bodyParser.urlencoded({extended: true}));
-
-/*app.post('/api/v1/image', (req, res) => {
-  console.log(req.files)
-      if(!req.files) {
-          res.send({
-              status: false,
-              message: 'No file uploaded!'
-          });
-      }
-      else {
-        ///Use the name of the input field (i.e. "avatar") to retrieve the uploaded file
-        let image = req.files.image;  
-        //Use the mv() method to place the file in upload directory (i.e. "uploads")
-       // image.mv('./uploads/' + image.name, (err, result) => {;
-     cloudinary.uploader.upload(image.tempFilePath, (result) => {
-    res.send({
-       message: 'File is uploaded',
-       success:true,
-       result: result.url
-      });
-       });
-      }
-      
-      });
-*/
-   
-  
+    
 app.get('/', (req, res) => {
     res.status(200).json({
       status: 200,
