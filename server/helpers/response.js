2fa61dcaa148b386a3651b29dc74d7ffe661f2c0
Lukaskov <sabin6341@gmail.com>
2019-07-10 12:04:02 +0200

    [ch-#167193531]update response file

--- server/helpers/response.js
+++ server/helpers/response.js
@@ -1,9 +1,5 @@
 
 
-const failed = (res, status, error) => res.status(status).send({ status, error });
-
-const success = (res, status, message,data) => res.status(status).send({ status, message,data});
-
-const successfull = (res, status, message,data) => res.status(status).send({ status, message});
-
-export default {failed,success,successfull};
+const server = (res, status,message,data) => res.status(status).send({ status,message,data});
+ 
+export default {server};
